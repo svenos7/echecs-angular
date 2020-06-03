075001cbc44340ca07de8ef60478b65d414179ee
svenos7 <sven_charmeteau@msn.com>
2020-06-03 09:58:41 +0200

    membre service

--- src/app/membre/service/membre.service.ts
+++ src/app/membre/service/membre.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
-import {Tournoi} from "../../model/tournoi";
 import {Membre} from "../model/membre";
 
 @Injectable({
